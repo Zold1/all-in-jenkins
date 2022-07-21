f398983e22b5d86f17daa471aef9e870f346e8d9
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2022-07-21 16:41:33 +0200

    Add Docker class

---
+++ src/containerization/Docker.groovy
@@ -0,0 +1,24 @@
+// package containerization
+
+class Docker implements Serializable {
+  def script
+  
+  Docker(script) {
+    this script = script
+  }
+  
+  def buildDockerImage(String imageName) {
+    script.echo "building the docker image..."
+    script.sh "docker build -t $imageName ."
+  }
+  
+  def dockerLogin() {
+    script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', usernameVariable: USER, passwordVariable: PASS)]) {
+      script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+    }
+  }
+  
+  def dockerPush(String $imageName) {
+    script.sh "docker push $imageName"
+  }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
+import Docker 
+
 def call(String imageName) {
-  echo "building the docker image..."
-  withCredentials([usernamePassword(credentialsId: 'docker-account', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-    sh "docker build -t $imageName ."
-    sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
-    sh "docker push $imageName"
-  }
+    return new Docker(this).buildDockerImage(imageName)
 }
