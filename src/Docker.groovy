6791ea64abfab896d50b47193559f060734e29dd
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2022-07-23 19:50:49 +0200

    Edit Docker

--- src/Docker.groovy
+++ src/Docker.groovy
@@ -12,15 +12,13 @@ class Docker implements Serializable {
   
   def login() {
     script.echo "logging in to docker..."
-    script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', usernameVariable: username, passwordVariable: password)]) {
-      script.sh "echo $script.password | docker login -u $script.username --password-stdin"
-      script.echo "building the docker image..."
-      script.sh "docker push $imageName"
+    sscript.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', usernameVariable: USER, passwordVariable: PASS)]) {
+      script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
     }
   }
   
   def push(String $imageName) {
     script.echo "pushing the docker image..."
-    //script.sh "docker push $imageName"
+    script.sh "docker push $imageName"
   }
 }
