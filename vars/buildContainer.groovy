3643e648f760de198199ab552da38a9a5620793b
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2022-07-24 12:47:19 +0200

    Edit Docker

--- vars/buildContainer.groovy
+++ vars/buildContainer.groovy
@@ -4,5 +4,11 @@ def call(String imageName) {
     /*def docker = new Docker(this)
     docker.build()
     docker.login()*/
-    return new Docker(this).buildDockerImage(imageName)
+    //return new Docker(this).buildDockerImage(imageName)
+    script.echo "building the docker image..."
+    script.withCredentials([script.usernamePassword(credentialsId: 'docker-account', usernameVariable: USER, passwordVariable: PASS)]) {
+      script.sh "docker build -t $imageName ."
+      script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+      script.sh "docker push $imageName"
+    }
 }
