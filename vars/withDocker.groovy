8a7134f7e5f5e813a8acabffae7806cf9c4d3c59
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2022-07-24 20:05:32 +0200

    Edit Docker

--- vars/withDocker.groovy
+++ vars/withDocker.groovy
@@ -1,7 +1,7 @@
 import Docker
 
 def call(String imageName) {
-  def docker = new Docker(imageName)
+  def docker = new Docker(this)
   docker.build(imageName)
   docker.login()
   docker.push(imageName)
